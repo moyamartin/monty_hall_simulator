db6f013b53816e439b2b4fe26ea9fcd8d63fff72
Martin Moya <moyamartin1@gmail.com>
2022-12-27 15:33:27 -0300

    Add MontyHall class

---
+++ inc/MontyHall.hpp
@@ -0,0 +1,55 @@
+///
+/// @file       MontyHall.hpp
+/// @brief      Contains the definition of class MontyHall which inherits from
+///             Person.hpp
+/// @version    0.0.1
+/// @date       26. Dec. 2022
+/// @author     Martin Moya
+///
+
+#pragma once
+
+#include <Person.hpp>
+
+class MontyHall : public Person
+{
+    public:
+        MontyHall(int max_door_number);
+        ~MontyHall() = default;
+        
+        ///
+        /// pick_door
+        /// @brief
+        ///     Picks a door that it is a goat
+        /// @return
+        ///     the number of the door picked that it is not a goat
+        ///
+        int pick_door();
+
+        ///
+        /// has_won
+        /// @brief
+        ///     This function checks if the chosen number from a Player is the
+        ///     same as the door that has a car
+        /// 
+        /// @return
+        ///     true if won, false if the user lost
+        bool has_won(int chosen_door_number);
+
+    private:
+        ///
+        /// _car_door_number
+        /// @brief
+        ///     This variable contains the number of the winning door, it is
+        ///     it is generated during the constructor
+        ///
+        int _car_door_number;
+
+        ///
+        /// _choosen_door
+        /// @brief
+        ///     This variable contains the number of the choosen door, it is
+        ///     always a door number different than _car_door_number
+        ///
+        int _choosen_door;
+};

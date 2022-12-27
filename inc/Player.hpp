f47ac23c1d6b6ed7c4a131d30d46dfd4d23a9d0e
Martin Moya <moyamartin1@gmail.com>
2022-12-27 15:33:18 -0300

    Add Player class

---
+++ inc/Player.hpp
@@ -0,0 +1,66 @@
+///
+/// @file       Player.hpp
+/// @brief      Contains the definition of class Player which inherits from
+///             Person.hpp
+/// @version    0.0.1
+/// @date       26. Dec. 2022
+/// @author     Martin Moya
+///
+
+#pragma once
+
+// stdlib/external dependencies
+
+
+// created dependencies
+#include <Person.hpp>
+
+
+class Player : public Person
+{
+public:
+    Player(const char* name, int max_door_number, bool repeat_door);
+    ~Player() = default;
+
+    ///
+    /// pick_door
+    /// @brief
+    ///     pick a door number randomly, the number can be between 0 and
+    ///     _max_range_val
+    /// @return
+    ///     the number that the player picked
+    ///
+    int pick_door();
+
+private:
+
+    ///
+    /// choosen_door
+    /// @brief
+    ///     here the result of pick_door is stored, later used when pick_door is
+    ///     ran for the second time
+    ///
+    int _choosen_door;
+
+    ///
+    /// repeat_door
+    /// @brief
+    ///     repeat_door is a boolean value set in the constructor of the
+    ///     variable
+    bool _repeat_door;
+
+    ///
+    /// _second_pick
+    /// @brief
+    ///     this variable when true states that it is the second pick of the
+    ///     player
+    ///
+    bool _second_pick;
+    
+    ///
+    /// _monty_hall_door
+    /// @brief
+    ///     this variable contains the number chosen by MontyHall class
+    ///
+    int _monty_hall_door;
+};  
---
+++ src/Player.cpp
@@ -0,0 +1,29 @@
+#include <Player.hpp>
+
+Player::Player(const char* name, int max_door_number, bool repeat_door) :
+    Person(name, 0, max_door_number), _choosen_door(-1), _monty_hall_door(-1),
+    _repeat_door(repeat_door), _second_pick(false)
+{
+}
+
+int Player::pick_door()
+{
+    // check if the player has done the second pick and it is configured to
+    // repeat the same door as the first pick
+    if(!_second_pick)
+    {
+        _choosen_door = generate_random_number();
+        _second_pick = true;
+    }
+    else
+    {
+        int tmp_door_number = _choosen_door; 
+        while(!_repeat_door && _choosen_door != tmp_door_number && 
+              _choosen_door != _monty_hall_door)
+        {
+            _choosen_door = generate_random_number();
+        }
+    }
+    return _choosen_door;
+}
+
